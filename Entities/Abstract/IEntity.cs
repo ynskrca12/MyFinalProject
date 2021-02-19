11f0e0ea7da574a2989897bb0aa41c45849fdead
ynskrca12 <74262367+ynskrca12@users.noreply.github.com>
2021-02-20 01:21:32 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //Ientity Implement eden class bir veritabanı tablosudur.
+    public interface IEntity
+    {
+
+    }
+}
